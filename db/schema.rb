7458e24fbff6d8830cb1c5184ccd087ce84c4539
Brie Coyle <brie.coyle@gmail.com>
2017-04-15 17:57:38 -0400

    Add index for lists.

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,8 @@ ActiveRecord::Schema.define(version: 20170415212128) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
