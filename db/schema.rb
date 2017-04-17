3d8cb1f8681f2065446a50fda5a13e64dae4882c
Brie Coyle <brie.coyle@gmail.com>
2017-04-17 18:27:03 -0400

    Add more data to users table indatabase.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417205827) do
+ActiveRecord::Schema.define(version: 20170417222612) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20170417205827) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
