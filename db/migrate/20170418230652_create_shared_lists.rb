a8460df33a483bb55d454399bbdfe514e3c60140
Brie Coyle <brie.coyle@gmail.com>
2017-04-18 21:46:57 -0400

    Replace hardcoded ids with activerecord associations.

--- db/migrate/20170418230652_create_shared_lists.rb
+++ db/migrate/20170418230652_create_shared_lists.rb
@@ -1,8 +1,8 @@
 class CreateSharedLists < ActiveRecord::Migration[5.0]
   def change
     create_table :shared_lists do |t|
-      t.integer :list_id
-      t.integer :user_id
+      t.belongs_to :list, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170419012750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "permission"
+    t.index ["list_id"], name: "index_shared_lists_on_list_id"
+    t.index ["user_id"], name: "index_shared_lists_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
