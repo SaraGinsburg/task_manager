49eba6df31c7053fe21b5dd41d15eb4384125ab8
briecoyle <brie.coyle@gmail.com>
2017-06-13 16:58:16 -0400

    Establish changes to index page after ajax request is completed.

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -11,7 +11,7 @@ function bindClickHandlers() {
         return response.json()
       })
       .then(function(list) {
-        console.log(list.tasks)
+        $(".tasks-container").html("hello")
       })
   })
 }
