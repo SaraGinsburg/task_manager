f39fe9f5e6e1acd630cad36ed7727a72c219771f
briecoyle <brie.coyle@gmail.com>
2017-07-04 18:44:24 -0400

    Render task information on lists index page with a model object.

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -7,12 +7,31 @@ function bindClickHandlers() {
     event.preventDefault();
     let id=$(this).attr('data-id')
     fetch(`/lists/${id}.json`, {credentials: 'include'})
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(list) {
-        console.log(list)
-        $(".tasks-container").html(list)
+      .then(response => response.json())
+      .then(list => {
+        $("#tasks-container").html("")
+        list.tasks.forEach(task => {
+          console.log(task)
+          let newTask = new Task(task);
+          let taskHtml = newTask.formatTask();
+
+          $(".tasks-container").append(taskHtml)
+        })
       })
   })
+}
+
+function Task(task) {
+  this.id = task.id
+  this.name = task.name
+  this.status = task.status
+  this.due_date = task.due_date
+  this.list_id = task.list_id
+}
+
+Task.prototype.formatTask = function() {
+  let taskHtml = `
+    <li class="${this.status}">${this.name}<li>
+  `
+  return taskHtml
 }
